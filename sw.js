ec2f2f66aa217f93812ee6a24bbd13da5528b03b
Kajewski, Matthew <matthew.kajewski@corteva.com>
2026-05-02 08:34:48 -0500

    v1.5.0 - ROLLBACK to v1.2.8 working code - all buttons work with inline handlers

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Pitching Tracker PWA
-const CACHE_VERSION = 'v1.2.8';
+const CACHE_VERSION = 'v1.5.0';
 const CACHE_NAME = `pitching-tracker-${CACHE_VERSION}`;
 
 const urlsToCache = [
