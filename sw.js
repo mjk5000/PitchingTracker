1d4b4f3e944db6b0c43dcb8c7c93537a23ae3d09
Kajewski, Matthew <matthew.kajewski@corteva.com>
2026-05-03 08:30:49 -0500

    v1.5.6 - Make inactive day columns narrower (70px) to save space, active column expands to 140px

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Pitching Tracker PWA
-const CACHE_VERSION = 'v1.5.5';
+const CACHE_VERSION = 'v1.5.6';
 const CACHE_NAME = `pitching-tracker-${CACHE_VERSION}`;
 
 const urlsToCache = [
