7481df5b5d5530ba0e7ce70073fa783e67c8f638
Kajewski, Matthew <matthew.kajewski@corteva.com>
2026-05-04 09:57:07 -0500

    v1.6.5 - Change Share icon to document/copy icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Pitching Tracker PWA
-const CACHE_VERSION = 'v1.6.4';
+const CACHE_VERSION = 'v1.6.5';
 const CACHE_NAME = `pitching-tracker-${CACHE_VERSION}`;
 
 const urlsToCache = [
