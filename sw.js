bad7d70997e24a2ecf2110bc84218e97036844c1
Kajewski, Matthew <matthew.kajewski@corteva.com>
2026-05-04 12:41:33 -0500

    v1.7.5 - Fix Quick Reference Rules and Settings headers to be non-selectable

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Pitching Tracker PWA
-const CACHE_VERSION = 'v1.7.4';
+const CACHE_VERSION = 'v1.7.5';
 const CACHE_NAME = `pitching-tracker-${CACHE_VERSION}`;
 
 const urlsToCache = [
