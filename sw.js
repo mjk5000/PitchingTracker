01c615df1fcbd60ce7dfeaad7748bfe5474fa052
Kajewski, Matthew <matthew.kajewski@corteva.com>
2026-05-05 09:55:08 -0500

    v1.8.8 - Restore version div gradient background with negative margin to eliminate gap

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Pitching Tracker PWA
-const CACHE_VERSION = 'v1.8.7';
+const CACHE_VERSION = 'v1.8.8';
 const CACHE_NAME = `pitching-tracker-${CACHE_VERSION}`;
 
 const urlsToCache = [
